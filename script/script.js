ddc01ddf344283a7aac9e8406401c2beb1e1e2b1
Thando <166392873+TechSavvy101@users.noreply.github.com>
2025-03-06 11:03:38 +0200

    Restructed_Restyled

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//Burger Menu
+const burgerMenu = document.getElementById('burgerMenu');
+const navLinks = document.getElementById('navLinks');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
